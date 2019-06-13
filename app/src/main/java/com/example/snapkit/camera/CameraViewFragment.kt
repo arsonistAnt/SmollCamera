4d57ce17f54716db5cd5441fbd9d175cc2e9bae8
arsonistAnt <anthonynguyen194@gmail.com>
2019-06-12 23:56:53 -0700

    Added preview image state and the cancel "X" button.
    - An image preview will now pop up when the user presses the capture image button.
    - The user can exit the image preview by pressing the "X" button that appears after image capture.
    - Added a new data state to the CameraViewModel to help observe when the user exits/enters the preview image state.

--- app/src/main/java/com/example/snapkit/camera/CameraViewFragment.kt
+++ app/src/main/java/com/example/snapkit/camera/CameraViewFragment.kt
@@ -2,11 +2,11 @@ package com.example.snapkit.camera
 
 import android.graphics.Bitmap
 import android.os.Bundle
-import android.util.Log
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
 import android.widget.Toast
+import androidx.core.graphics.drawable.toBitmap
 import androidx.fragment.app.Fragment
 import androidx.lifecycle.Observer
 import androidx.lifecycle.ViewModelProviders
@@ -50,16 +50,48 @@ class CameraViewFragment : Fragment() {
         camera.setLifecycleOwner(viewLifecycleOwner)
         // Override on picture taken CameraListener
         camera.addCameraListener(object : CameraListener() {
+            //TODO: Handle when user interrupts snapshot process via home button or swiping up on the home button.
             override fun onPictureTaken(result: PictureResult) {
                 // Picture was taken!
-//                result.toBitmap(BitmapCallback { convertedBitmap ->
-//                    capturedImage = convertedBitmap!!
-//                })
-                Toast.makeText(activity!!.applicationContext, "Image capture button clicked!", Toast.LENGTH_SHORT)
-                    .show()
+                result.data
+                result.toBitmap { convertedBitmap ->
+                    capturedImage = convertedBitmap!!
+
+                    // Show the user the captured image in the imagePreview view.
+                    binding.imagePreview.setImageBitmap(capturedImage)
+                }
+
+                // Switch to the preview image state after the picture has been taken.
+                viewModel.onPreviewImageState()
             }
         })
     }
+
+    /**
+     * Show the buttons and the preview image itself to the user in the image preview state of the app.
+     * By default if called without an argument, the function will set the visibility of the UI to VISIBLE.
+     * @param hide a boolean that decides whether the buttons in the preview state should be shown.
+     */
+    private fun showImagePreviewUI(show: Boolean = true) {
+        if (show) {
+            binding.imagePreview.visibility = View.VISIBLE
+            binding.exitPreviewButton.visibility = View.VISIBLE
+            binding.buttonCaptureImage.visibility = View.GONE
+        } else {
+            binding.imagePreview.apply {
+                visibility = View.GONE
+                // Must remove current bitmap from ImageView otherwise the user will see
+                // previous images from past captures.
+                setImageBitmap(null)
+            }
+            binding.exitPreviewButton.visibility = View.GONE
+            binding.buttonCaptureImage.visibility = View.VISIBLE
+            // Recycle unused bitmap.
+            capturedImage?.recycle()
+        }
+
+    }
+
 
     /**
      * Setup the observers to watch the CameraViewModel data.
@@ -73,5 +105,18 @@ class CameraViewFragment : Fragment() {
                 viewModel.onCaptureButtonFinished()
             }
         })
+
+        // Tell the fragment to handle the preview UI when the user in the image preview state.
+        viewModel.inImagePreviewState.observe(viewLifecycleOwner, Observer { finishCapturedState ->
+            if (finishCapturedState == true) {
+                showImagePreviewUI()
+            } else {
+                // This case is reached when the user presses the "X" button in the preview UI.
+                showImagePreviewUI(false)
+            }
+
+        })
     }
+
+
 }
--- app/src/main/java/com/example/snapkit/camera/CameraViewModel.kt
+++ app/src/main/java/com/example/snapkit/camera/CameraViewModel.kt
@@ -5,12 +5,17 @@ import androidx.lifecycle.MutableLiveData
 import androidx.lifecycle.ViewModel
 
 class CameraViewModel : ViewModel() {
-    // If the user has clicked on the snap shot button.
+    // If the user has clicked on the capture image button.
     private val _captureImage = MutableLiveData<Boolean>()
     val captureImageState: LiveData<Boolean>
         get() = _captureImage
 
-    //TODO: Keep a state where user wants to save the image that has been captured.
+    // If the user has just finished pressing the capture image button and are in the image preview state.
+    private val _inImagePreviewState = MutableLiveData<Boolean>()
+    val inImagePreviewState: LiveData<Boolean>
+        get() = _inImagePreviewState
+
+    //TODO: Save bitMap that has been captured.
     //TODO: Keep a state where user wants to navigate to the gallery by a button click.
 
     /**
@@ -26,4 +31,19 @@ class CameraViewModel : ViewModel() {
     fun onCaptureButtonFinished() {
         _captureImage.value = false
     }
+
+    /**
+     * Set _inImagePreviewState to true when user is in the preview image state.
+     */
+    fun onPreviewImageState() {
+        _inImagePreviewState.value = true
+    }
+
+    /**
+     * Set _inImagePreviewState to false when user is finished with the preview state.
+     */
+    fun onPreviewImageStateFinished() {
+        _inImagePreviewState.value = false
+    }
+
 }
